4a8882729d7755227c73c790ade0058b52101ca9
234Kavya <234kavya@gmail.com>
2024-08-31 11:34:06 +0530

    feat: implement addBook() method in Library class

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -9,6 +9,11 @@ public class Library {
         books = new ArrayList<>();
     }
 
+    // Add a new book to the library
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
     // Method to get the count of books in the library
     public int getBooksCount() {
         return books.size();
